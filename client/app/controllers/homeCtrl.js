f95635e83e82e015049ae6b04d81a9a74c42800c
John Hamm <johndhamm1@gmail.com>
2017-03-11 21:32:19 -0600

    test buttons/array setup with currentAthOrder change function

--- client/app/controllers/homeCtrl.js
+++ client/app/controllers/homeCtrl.js
@@ -13,6 +13,31 @@ app.controller("homeCtrl", function($scope, $location){
 			'name': 'three'}
 	]
 
+	let currentAthleteOrder = [ 0, 1, 2, 3 ],
+			athleteBtnHeight = 60,
+			numAthletes = $scope.testArray.length;
+
+
 	console.log("$scope.testArray", $scope.testArray);
+	console.log("numAthletes", numAthletes);
+
+	$scope.recordLap = (index) => {
+		console.log("btn index", index);
+
+		updateOrder(index);
+	}
+
+	const updateOrder = (index) => {
+		let orderIndex = findAthInCurrentOrder(index);
+		console.log("orderIndex", orderIndex);
+		currentAthleteOrder.splice(orderIndex, 1);
+		currentAthleteOrder.push(index);
+		console.log("currentAthleteOrder", currentAthleteOrder);
+	}
+
+	const findAthInCurrentOrder = (index) => {
+		return currentAthleteOrder.indexOf(index);
+	}
+
 
 });
